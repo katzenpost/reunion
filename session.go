f39c6c93dffb09530eca4d590759884677e6ec4e
David Stainton <dstainton415@gmail.com>
2024-08-26 17:07:49 -0700

    Add t1 encoding

--- session.go
+++ session.go
@@ -8,9 +8,33 @@ import (
 // "github.com/katzenpost/hpqc/nike/x25519"
 )
 
+const (
+	alphaLen = 32
+	betaLen  = 128
+	gammaLen = 16
+)
+
 type T1 struct {
-	Alpha [32]byte  // X25519 pub key
-	Beta  [128]byte // CTIDH 1024 pub key
-	Gamme [16]byte  // MAC
-	Delta []byte    // ciphertext
+	Alpha [alphaLen]byte // X25519 pub key
+	Beta  [betaLen]byte  // CTIDH 1024 pub key
+	Gamma [gammaLen]byte // MAC
+	Delta []byte         // ciphertext
+}
+
+func (t *T1) MarshalBinary() (data []byte, err error) {
+	out := []byte{}
+	out = append(out, t.Alpha[:]...)
+	out = append(out, t.Beta[:]...)
+	out = append(out, t.Gamma[:]...)
+	out = append(out, t.Delta...)
+	return out, nil
+}
+
+func (t *T1) UnmarshalBinary(data []byte) error {
+	copy(t.Alpha[:], data[:alphaLen])
+	copy(t.Beta[:], data[alphaLen:alphaLen+betaLen])
+	copy(t.Gamma[:], data[alphaLen+betaLen:alphaLen+betaLen+gammaLen])
+	t.Delta = make([]byte, len(data[alphaLen+betaLen+gammaLen:]))
+	copy(t.Delta[:], data[alphaLen+betaLen+gammaLen:])
+	return nil
 }
