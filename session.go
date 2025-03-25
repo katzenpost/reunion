ffd6b98f958c429c83865db3198398749888879c
David Stainton <dstainton415@gmail.com>
2025-03-24 20:25:00 -0700

    Add client/server request/response types

--- session.go
+++ session.go
@@ -26,6 +26,50 @@ const (
 	Type3MessageSize = 32
 )
 
+// ReunionRequest is a type for encapsulating all possible messages
+// sent by clients to the Reunion service on the mixnet.
+type ReunionRequest struct {
+
+	// Context contains the one week shared random value known as the weekly SRV for Katzenpost and it can never be nil.
+	Context [32]byte
+
+	// id contains a random 32 byte value to uniquely identify the request
+	id [32]byte
+
+	// T1 field can be populated witha T1 or it can be nil.
+	T1 *T1
+
+	// T2 field can be populated with a T2 or it can be nil.
+	T2 []byte
+
+	// T3 field can be populated with a T3 or it can be nil.
+	T3 []byte
+}
+
+//
+
+// ReunionResponse is type for encapsulating all possible messages
+// sent by the Reunion service to the clients via Sphinx SURB reply.
+type ReunionResponse struct {
+	// Context contains the one week shared random value known as the weekly SRV for Katzenpost and it can never be nil.
+	Context [32]byte
+
+	// id contains a random 32 byte value to uniquely identify the response that corresponds to the original ReunionRequest
+	id [32]byte
+
+	// ErrorCode indicates a specific error or status OK. Zero is success, one is error.
+	ErrorCode uint8
+
+	// T1 is a list of all T1 mesages on the server
+	T1 []*T1
+
+	// T2 is a list of all T2 messages on the server
+	T2 [][]byte
+
+	// T3 is a list of all T3 messages on the server
+	T3 [][]byte
+}
+
 // T1 is the first protocol message that contains the message to be sent as
 // well as relevant cryptographic information.
 // T1 message that consists of an Alpha, Beta, Gamma, and Delta items as bytes.
